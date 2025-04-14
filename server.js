dd2e1e54b82dfe8ead2d722bd2414dd3f8684ce1
Zavelitskaya Anastasiya <111677854+zavelitskaya@users.noreply.github.com>
2025-04-14 18:18:38 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,15 @@
+// /server.js
+const http = require("http");
+
+const HOST = 'localhost';
+const PORT = 8000;
+
+const handler = (req, res) => {
+    res.writeHead(200);
+    res.end('JavaScript - классный язык программирования!');
+};
+
+const server = http.createServer(handler);
+server.listen(PORT, HOST, () => {
+    console.log(`Сервер запущен по адресу http://${HOST}:${PORT}`);
+});
